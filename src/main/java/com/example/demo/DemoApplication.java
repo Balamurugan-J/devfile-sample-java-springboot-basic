a6e2e0e62e0c771be9ce3f1348222567ffb92489
Balamurugan J <40036301+Balamurugan-J@users.noreply.github.com>
2022-11-11 19:03:36 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World! Bala here";
+        return "Hello World! Bala here version 1";
     }
 
     public static void main(String[] args) {
